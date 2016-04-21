699b213f8f08fbb4f6246816dbcfab4be86fe7f2
猪野 湧斗 <ice.softicon30th@gmail.com>
2016-04-21 22:46:41 +0900

    private

--- csine-validator.js
+++ csine-validator.js
@@ -1,16 +1,39 @@
 var mongojs = require('mongojs');
 var db = mongojs('YSFHcSINE', ['session']);
 
-function createSession(user_id, password) {
-    var data = {
-        user_id: user_id,
-        password: password
-    };
+function validateSession(session_id, callback) {
+    var expires = new Date();
+    expires.setHours(expires.getHours() + 1);
     
-    db.session.insert(data, function(err, doc) {
-        console.log(doc)
+    db.session.update({session_id: session_id}, {$set: {expires: expires} }, function (err, result) {
+        // { ok: 1, nModified: 0, n: 0 }
+        if (err) {
+            console.error(err.stack);
+            callback(null);
+            return;
+        }
+        
+        if (result.nModified === 1) {
+            callback(expires);
+        } else if (result.nModified === 0) {
+            callback(null);
+        } else {
+            console.error('ERROR: Detected overlap of session_id.');
+            callback(null);
+        }
     });
-    
 }
 
-createSession('AAA', 'BBB');
+function sessionValidator(req, res, next) {
+    if (!req.cookies) throw new Error('cookie parser required');
+    validateSession(req.cookies.session_id, function (expires) {
+        if (!expires) {
+            next(new Error('Invalid Session'));
+        } else {
+            res.cookie('session_id', req.cookies.session_id, {expires: expires});
+            next();
+        }
+    });
+}
+
+module.exports = sessionValidator;
